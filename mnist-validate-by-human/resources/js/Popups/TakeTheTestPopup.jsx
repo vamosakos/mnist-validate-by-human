3f71d9aed9ddd4eff657265f081a0b4d26c62dec
vamosmarton <vammarton@gmail.com>
2024-02-27 01:54:51 +0100

    recaptcha panel alignment to the bottom middle

--- mnist-validate-by-human/resources/js/Popups/TakeTheTestPopup.jsx
+++ mnist-validate-by-human/resources/js/Popups/TakeTheTestPopup.jsx
@@ -27,13 +27,8 @@ export default function TakeTheTestPopup({ show, onClose }) {
             <div className="p-6">
                 <p>You will see 10 pictures, decide what number you see.</p>
                 <p>Would you like to start?</p>
-                <div className="mt-4 flex justify-center">
-                    {/* Render reCAPTCHA component */}
-                    <ReCAPTCHA
-                        sitekey="6Le0v38pAAAAAJ8F0jvrasL3E1VcEm3ikoUk7Wm9"
-                        onChange={handleCaptchaChange}
-                    />
-                    <div className="ml-4 mt-4"> {/* Add margin-left to this div */}
+                <div className="mt-4 flex justify-center flex-col items-center">
+                    <div className="flex">
                         <Link href={route('test')}>
                             <button
                                 className={`bg-green-custom text-white rounded-full font-bold py-2 px-4 hover:bg-emerald-600 mr-4 ${!captchaValue && 'cursor-not-allowed opacity-50'}`}
@@ -45,12 +40,19 @@ export default function TakeTheTestPopup({ show, onClose }) {
                         </Link>
                         <Link>
                             <button
-                                className="bg-red-500 text-white rounded-full font-bold py-2 px-4 hover:bg-red-700 mr-4"
+                                className="bg-red-500 text-white rounded-full font-bold py-2 px-4 hover:bg-red-700"
                                 onClick={onClose}
                             >
                                 No
                             </button>
                         </Link>
+                    </div>
+                    {/* Render reCAPTCHA component */}
+                    <div className="mt-4">
+                        <ReCAPTCHA
+                            sitekey="6Le0v38pAAAAAJ8F0jvrasL3E1VcEm3ikoUk7Wm9"
+                            onChange={handleCaptchaChange}
+                        />
                     </div>
                 </div>
             </div>
